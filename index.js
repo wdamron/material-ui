e9b87a9d6170927801702552ed3b5e1552236f6c
Christian Sullivan <cs@bodhi5.com>
2014-11-13 16:16:22 -0800

    switch to b5 react

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  AppBar: require('./lib/app-bar'),
+  AppCanvas: require('./lib/app-canvas'),
+  Checkbox: require('./lib/checkbox'),
+  Dialog: require('./lib/dialog'),
+  DropDownIcon: require('./lib/drop-down-icon'),
+  DropDownMenu: require('./lib/drop-down-menu'),
+  Icon: require('./lib/icon'),
+  Input: require('./lib/input'),
+  LeftNav: require('./lib/left-nav'),
+  MenuItem: require('./lib/menu-item'),
+  Menu: require('./lib/menu'),
+  Mixins: {
+    Classable: require('./lib/mixins/classable'),
+    ClickAwayable: require('./lib/mixins/click-awayable')
+  },
+  PaperButton: require('./lib/paper-button'),
+  Paper: require('./lib/paper'),
+  RadioButton: require('./lib/radio-button'),
+  Ripple: require('./lib/ripple'),
+  Toggle: require('./lib/toggle'),
+  Toast: require('./lib/toast'),
+  Toolbar: require('./lib/toolbar'),
+  ToolbarGroup: require('./lib/toolbar-group')
+};
